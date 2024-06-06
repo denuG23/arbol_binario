3828ee2547a1b6c40b8c0b35fdd813a62bfe50eb
nissegm <nissgonzalezm@gmail.com>
2024-06-06 00:15:20 -0300

    primer commit

---
+++ trabajo practico 1/ArbolBinario.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Denu
+ * Date: 1/4/2024
+ * Time: 20:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace trabajo_practico_1
+{
+	/// <summary>
+	/// Description of ArbolBinario
+
+	public class ArbolBinario<T>
+	{
+		private T dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+
+		public ArbolBinario(T dato)
+		{
+			this.dato = dato;
+			this.hijoIzquierdo = null;
+			this.hijoDerecho = null;
+		}
+
+		public T getDatoRaiz()
+		{
+			return this.dato;
+		}
+		
+		public void setDatoRaiz(T dato)
+		{
+			this.dato = dato;
+		}
+
+		public ArbolBinario<T> getHijoIzquierdo()
+		{
+			return this.hijoIzquierdo;
+		}
+		
+		public void setHijoIzquierdo(ArbolBinario<T> hijoIzquierdo)
+		{
+			this.hijoIzquierdo = hijoIzquierdo;
+		}
+
+		public ArbolBinario<T> getHijoDerecho()
+		{
+			return this.hijoDerecho;
+		}
+		
+		public void setHijoDerecho(ArbolBinario<T> hijoDerecho)
+		{
+			this.hijoDerecho = hijoDerecho;
+		}
+
+
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo)
+		{
+			this.hijoIzquierdo = hijo;
+		}
+
+		public void agregarHijoDerecho(ArbolBinario<T> hijo)
+		{
+			this.hijoDerecho = hijo;
+		}
+
+		public void eliminarHijoIzquierdo()
+		{
+			this.hijoIzquierdo = null;
+		}
+
+		public void eliminarHijoDerecho()
+		{
+			this.hijoDerecho = null;
+		}
+
+		public bool esHoja()
+		{
+			return this.hijoIzquierdo == null && this.hijoDerecho == null;
+		}
+
+		public bool buscarCamino(ArbolBinario<T> arbol, int longitud, List<T> camino)
+		{
+			if (arbol == null)
+				return false;
+
+			if (longitud == 0 && arbol.esHoja())
+			{
+				camino.Add(arbol.getDatoRaiz());
+				return true;
+			}
+
+			if (longitud > 0)
+			{
+				if (arbol.getHijoIzquierdo() != null && buscarCamino(arbol.getHijoIzquierdo(), longitud - 1, camino))
+				{
+					camino.Insert(0, arbol.getDatoRaiz());
+					return true;
+				}
+
+				if (arbol.getHijoDerecho() != null && buscarCamino(arbol.getHijoDerecho(), longitud - 1, camino))
+				{
+					camino.Insert(0, arbol.getDatoRaiz());
+					return true;
+				}
+			}
+
+			return false;
+		}
+		public List<T> resolver(ArbolBinario<T> arbol, int longitud)
+		{
+			List<T> camino = new List<T>();
+			bool encontrado = buscarCamino(arbol, longitud, camino);
+			return camino;
+		}
+	}
+}
---
+++ trabajo practico 1/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Denu
+ * Date: 1/4/2024
+ * Time: 20:14
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace trabajo_practico_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			//prueba arbol binario del ejemplo
+			ArbolBinario<int> arbolBinario1 = new ArbolBinario<int>(8);
+            ArbolBinario<int> hijoIzquierdo = new ArbolBinario<int>(5);
+            ArbolBinario<int> hijoDerecho = new ArbolBinario<int>(22);
+            ArbolBinario<int> hijoIzq_Derecho = new ArbolBinario<int>(6);
+            hijoDerecho.agregarHijoIzquierdo(hijoIzq_Derecho );
+            hijoIzq_Derecho .agregarHijoIzquierdo(new ArbolBinario<int>(7));
+            hijoDerecho.agregarHijoDerecho(new ArbolBinario<int>(18));
+
+            arbolBinario1.agregarHijoIzquierdo(hijoIzquierdo);
+            arbolBinario1.agregarHijoDerecho(hijoDerecho);
+
+            Console.WriteLine("Longitud 1: " + string.Join("-", arbolBinario1.resolver(arbolBinario1, 1)));
+            Console.WriteLine("Longitud 2: " + string.Join("-", arbolBinario1.resolver(arbolBinario1, 2)));
+            Console.WriteLine("Longitud 5: " + string.Join("-", arbolBinario1.resolver(arbolBinario1, 5)));
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+		}
+	}
+}
